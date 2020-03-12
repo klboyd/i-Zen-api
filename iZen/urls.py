48c7404578e880ad54952ac686e82150ee851dd0
Ken Boyd <klb417@gmail.com>
2020-03-12 12:05:57 -0500

    added user view and serializer, login, and register

--- iZen/urls.py
+++ iZen/urls.py
@@ -17,13 +17,16 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import include, path
 from iZenAPI.models import *
+from iZenAPI.views import login_user, register_user, Users
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+router.register(r"users", Users, "user")
+
 urlpatterns = [
     path("", include(router.urls)),
-    # path("register", register_user),
-    # path("login", login_user),
+    path("register", register_user),
+    path("login", login_user),
     path("api-token-auth/", obtain_auth_token),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
