ad7d2b3c5f12f69eedd5967cf83033c712467f1d
Ken Boyd <klb417@gmail.com>
2020-03-12 13:52:55 -0500

    added noteboard view and serializer

--- iZen/settings.py
+++ iZen/settings.py
@@ -49,7 +49,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         # 'rest_framework.permissions.AllowAny',
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        # "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.permissions.IsAuthenticated"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
